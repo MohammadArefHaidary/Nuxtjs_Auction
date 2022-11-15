7c79d3bc7b22e594113a8ef60b19458f94bd6324
Mohammad Aref <mohammadaref.haidary1234@gmail.com>
2022-11-15 15:26:53 +0330

    feat:test

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1> {{ users }} </h1>
+</template>
+
+<script>
+import axios from "axios";
+import { ref } from "vue";
+export default {
+  setup() {
+    const users = ref([]);
+
+    function getUsers() {
+      axios
+
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
+          users.value = response.data;
+        });
+    }
+    getUsers();
+
+    return { users };
+  },
+};
+</script>
