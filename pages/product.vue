be880a3ecda227af9f752583dc99d05534bacb11
Mohammad Aref <mohammadaref.haidary1234@gmail.com>
2022-11-30 11:25:22 +0330

    fix:the product materials icon fixed

--- pages/product.vue
+++ pages/product.vue
@@ -122,7 +122,7 @@
                   </a>
                 </div>
               </div>
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
               <div class="my-2">
                 <div class="lnks d-flex align-items-center">
                   <div>
