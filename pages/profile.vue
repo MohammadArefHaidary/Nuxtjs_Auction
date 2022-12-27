07ea32bd8c38e40df8b3416441c8273a369cc0be
Mohammad Aref <mohammadaref.haidary1234@gmail.com>
2022-12-27 14:49:18 +0330

    fix:mobile size of profile done

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="back_image " ></div>
-    <div class="d-flex flex-direction-row-reverse justify-content-between align-items-center mt-5">
+    <div class="d-flex flex-direction-row-reverse justify-content-between align-items-center mg">
       <div style="z-index:1;margin-right:100px;">
         <img src="~/assets/images/pexels-photo-1448136.jpeg" alt=""  
         style="width:115px;height:115px;border-radius:50%;">
@@ -12,14 +12,14 @@
       </div>
     </div>
 
-    <div class="d-flex justify-content-between">
+    <div class="d-flex justify-content-between flex-wrap">
       <div style="margin-right:100px;">
         <div>محمد</div>
         <div class="d-flex flex-row-reverse"><div style="margin-right:10px"><button class="btn_style">0*1233...1334</button></div><div><button class="btn_style">ادیت پروفایل</button></div></div>
         <div class="mt-2">دیزاینر</div>
         <div>وب دیزاینر</div>
       </div>
-      <div style="margin-left:100px" class="d-flex">
+      <div style="margin-left:100px" class="d-flex mgr">
         <div >تعقیب کننده
           <p>100</p>
         </div>
